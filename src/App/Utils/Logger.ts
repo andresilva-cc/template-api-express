63fa5d0a69402a69181d384c9bda4e79769e2d1f
André Luiz <andreluiz.97@outlook.com>
2021-08-02 16:03:57 -0300

    feat: Logger

---
+++ src/App/Utils/Logger.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-console */
+
+import symbols from 'log-symbols';
+import chalk from 'chalk';
+import { APP_ENV } from '../../Config/app';
+
+export default class Logger {
+  static info(message: string, environments = ['production', 'development', 'test']) {
+    if (environments.includes(APP_ENV || 'production')) {
+      console.log(symbols.info, chalk.blue(message));
+    }
+  }
+
+  static success(message: string, environments = ['production', 'development', 'test']) {
+    if (environments.includes(APP_ENV || 'production')) {
+      console.log(symbols.success, chalk.green(message));
+    }
+  }
+
+  static warning(message: string, environments = ['production', 'development', 'test']) {
+    if (environments.includes(APP_ENV || 'production')) {
+      console.log(symbols.warning, chalk.yellow(message));
+    }
+  }
+
+  static error(message: string, environments = ['production', 'development', 'test']) {
+    if (environments.includes(APP_ENV || 'production')) {
+      console.log(symbols.error, chalk.red(message));
+    }
+  }
+}
