97d88c91c8b0191a50da5a83aee7c4d085ed23ab
André Luiz <andreluiz.97@outlook.com>
2021-08-02 21:57:18 -0300

    refactor: Port as a string

--- src/Config/app.ts
+++ src/Config/app.ts
@@ -4,9 +4,9 @@ export const APP_URL = process.env.APP_URL || 'http://api.myapp.com';
 
 export const APP_USE_HTTPS = Boolean(process.env.APP_USE_HTTPS) || false;
 
-export const APP_HTTP_PORT = parseInt(process.env.APP_HTTP_PORT || '80', 10);
+export const APP_HTTP_PORT = process.env.APP_HTTP_PORT || '80';
 
-export const APP_HTTPS_PORT = parseInt(process.env.APP_HTTPS_PORT || '443', 10);
+export const APP_HTTPS_PORT = process.env.APP_HTTPS_PORT || '443';
 
 export const APP_SSL_KEY = process.env.APP_SSL_KEY || '';
 
