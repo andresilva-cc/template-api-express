ac8a74c6a1ae07148f1a4b73a4058d65782dc7c9
André Luiz <andreluiz.97@outlook.com>
2021-08-10 18:06:28 -0300

    fix: Add missing decorators to User model

--- src/App/Models/User.ts
+++ src/App/Models/User.ts
@@ -1,19 +1,23 @@
 import {
-  Table, Column, Model, CreatedAt, UpdatedAt, IsEmail, Length,
+  Table, Column, Model, CreatedAt, UpdatedAt, IsEmail, Length, AllowNull, Unique,
 } from 'sequelize-typescript';
 
 @Table
 export default class User extends Model {
   @Length({ min: 3, max: 255 })
+  @AllowNull(false)
   @Column
   name!: string;
 
   @IsEmail
   @Length({ max: 254 })
+  @Unique
+  @AllowNull(false)
   @Column
   email!: string;
 
   @Length({ max: 60 })
+  @AllowNull(false)
   @Column
   password!: string;
 
