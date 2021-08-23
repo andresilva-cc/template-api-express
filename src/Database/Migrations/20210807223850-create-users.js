4942240e2b29cbaab7999acd4cadc73474b99452
André Luiz <andreluiz.97@outlook.com>
2021-08-23 19:07:32 -0300

    fix: Migration JSDoc and correct return

--- src/Database/Migrations/20210807223850-create-users.js
+++ src/Database/Migrations/20210807223850-create-users.js
@@ -8,44 +8,40 @@ module.exports = {
    * @param {Sequelize.QueryInterface} queryInterface
    * @param {Sequelize.DataTypes} types
    */
-  up: async (queryInterface, types) => {
-    queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: types.INTEGER,
-      },
-      name: {
-        allowNull: false,
-        type: types.STRING,
-      },
-      email: {
-        allowNull: false,
-        type: types.STRING(254),
-        unique: true,
-      },
-      password: {
-        allowNull: false,
-        type: types.STRING(60),
-      },
-      createdAt: {
-        allowNull: false,
-        type: types.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: types.DATE,
-      },
-    });
-  },
+  up: async (queryInterface, types) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: types.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: types.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: types.STRING(254),
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: types.STRING(60),
+    },
+    createdAt: {
+      allowNull: false,
+      type: types.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: types.DATE,
+    },
+  }),
 
   /**
    * Undo the migration
    *
-   * @param {QueryInterface} queryInterface
+   * @param {Sequelize.QueryInterface} queryInterface
    */
-  down: async (queryInterface) => {
-    queryInterface.dropTable('Users');
-  },
+  down: async (queryInterface) => queryInterface.dropTable('Users'),
 };
