d762fdc67587fdaee44c65c87b6eead372a24a0a
André Luiz <andreluiz.97@outlook.com>
2021-08-28 23:25:34 -0300

    refactor: Error middleware

--- src/Middlewares/errorMiddleware.ts
+++ src/Middlewares/errorMiddleware.ts
@@ -2,15 +2,25 @@ import { Request, Response, NextFunction } from 'express';
 import BaseError from '../App/Errors/BaseError';
 import ErrorParser from '../App/Errors/ErrorParser';
 
+const sendErrorResponse = (error: any, response: Response) => response
+  .status(error.code)
+  .send(error.toPlainObject());
+
+const parseErrorAndSendResponse = (error: any, response: Response) => {
+  const parsedError = ErrorParser.parse(error);
+  return sendErrorResponse(parsedError, response);
+};
+
+const isErrorProperlyFormatted = (error: any) => error instanceof BaseError;
+
 export default (error: any, _request: Request, response: Response, next: NextFunction) => {
   if (!error) {
     return next();
   }
 
-  if (error instanceof BaseError) {
-    return response.status(error.code).send(error.toPlainObject());
+  if (isErrorProperlyFormatted(error)) {
+    return sendErrorResponse(error, response);
   }
 
-  const parsedError = ErrorParser.parse(error);
-  return response.status(parsedError.code).send(parsedError.toPlainObject());
+  return parseErrorAndSendResponse(error, response);
 };
