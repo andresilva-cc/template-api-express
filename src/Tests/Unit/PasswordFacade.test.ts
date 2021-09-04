019e7d21964071c0c1dfe37666fa0c9e9743b689
André Luiz <andreluiz.97@outlook.com>
2021-09-04 17:34:42 -0300

    test: PasswordFacade

---
+++ src/Tests/Unit/PasswordFacade.test.ts
@@ -0,0 +1,17 @@
+import { PasswordFacade } from '../../App/Facades';
+
+describe('PasswordFacade', () => {
+  beforeAll(() => {
+    process.env.AUTH_SECRET = 'secret_token';
+  });
+
+  it('should return a 60 length string hash', () => {
+    expect(PasswordFacade.hash('password').length).toBe(60);
+  });
+
+  test('password compare', () => {
+    const hash = PasswordFacade.hash('secure_password');
+
+    expect(PasswordFacade.compare('secure_password', hash)).toBeTruthy();
+  });
+});
