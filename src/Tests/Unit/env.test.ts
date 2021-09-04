d15f78267ac2746728dec62da1da2a33074e9f5c
André Luiz <andreluiz.97@outlook.com>
2021-09-04 17:33:31 -0300

    test: env

---
+++ src/Tests/Unit/env.test.ts
@@ -0,0 +1,13 @@
+import env from '../../App/Utils/env';
+
+describe('env', () => {
+  it('should return the defined value', () => {
+    process.env.NONEXISTENT = 'aValue';
+
+    expect(env('NONEXISTENT', 'defaultValue')).toBe('aValue');
+  });
+
+  it('should return the default value', () => {
+    expect(env('ANOTHER_KEY', 'defaultValue')).toBe('defaultValue');
+  });
+});
