4f8c5d269730aeeea5dab36c6f0d8ae5eb4b1672
André Luiz <andreluiz.97@outlook.com>
2021-09-04 20:09:53 -0300

    test: TokenFacade

---
+++ src/Tests/Unit/TokenFacade.test.ts
@@ -0,0 +1,48 @@
+import { JsonWebTokenError, JwtPayload } from 'jsonwebtoken';
+import { TokenFacade } from '../../App/Facades';
+
+describe('TokenFacade', () => {
+  let token: string;
+
+  beforeAll(async () => {
+    process.env.AUTH_SECRET = 'secret_token';
+    token = await TokenFacade.sign({
+      email: 'user@test.com',
+    });
+  });
+
+  it('should generate a token', async () => {
+    expect(typeof token).toBe('string');
+    expect(token).toBeTruthy();
+  });
+
+  it('should contain email property on the payload', async () => {
+    // @ts-ignore
+    expect(await TokenFacade.verify(token).email).toBe('user@test.com');
+  });
+
+  test('audience match', async () => {
+    const tokenWithAudience = await TokenFacade.sign({}, {
+      audience: 'app',
+    });
+
+    expect((await TokenFacade.verify(tokenWithAudience, 'app') as JwtPayload).aud).toBe('app');
+  });
+
+  test('audience shouldn\'t match', async () => {
+    const tokenWithAudience = await TokenFacade.sign({}, {
+      audience: 'api',
+    });
+
+    // For some reason, .rejects.toThrow() does not work here
+    let errorObject;
+
+    try {
+      TokenFacade.verify(tokenWithAudience, 'app');
+    } catch (error) {
+      errorObject = error;
+    }
+
+    expect(errorObject).toBeInstanceOf(JsonWebTokenError);
+  });
+});
