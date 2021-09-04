001944d6feb61c2598523e986486cbea3bae404e
André Luiz <andreluiz.97@outlook.com>
2021-09-04 17:33:58 -0300

    test: Logger

---
+++ src/Tests/Unit/Logger.test.ts
@@ -0,0 +1,35 @@
+/* eslint-disable no-console */
+
+import Logger from '../../App/Utils/Logger';
+
+describe('Logger', () => {
+  beforeAll(() => {
+    console.log = jest.fn();
+    process.env.APP_ENV = 'test';
+  });
+
+  it('should log the text "Server started"', () => {
+    Logger.info('Server started');
+
+    expect(String((console.log as jest.Mock).mock.calls[0])).toContain(('Server started'));
+  });
+
+  it('should log two parameters (icon and text)', () => {
+    Logger.success('Server started');
+
+    expect((console.log as jest.Mock).mock.calls[0].length).toBe(2);
+  });
+
+  test('both parameters are string', () => {
+    Logger.warning('Be careful!');
+
+    expect(typeof (console.log as jest.Mock).mock.calls[0][0]).toBe('string');
+    expect(typeof (console.log as jest.Mock).mock.calls[0][1]).toBe('string');
+  });
+
+  it('should log icon', () => {
+    Logger.error('Fatal error');
+
+    expect((console.log as jest.Mock).mock.calls[0][0].length).not.toBe('Fatal error');
+  });
+});
