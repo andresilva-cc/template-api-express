0a0a9326ced037b11a9a2c6a39fcf9e20004a7f2
André Luiz <andreluiz.97@outlook.com>
2021-09-04 20:09:47 -0300

    fix: Audience as optional on TokenFacade

--- src/App/Facades/TokenFacade.ts
+++ src/App/Facades/TokenFacade.ts
@@ -6,7 +6,7 @@ export default class JWTFacade {
     return jwt.sign(payload, AUTH_SECRET, options);
   }
 
-  static verify(token: string, audience: string) {
+  static verify(token: string, audience?: string) {
     return jwt.verify(token, AUTH_SECRET, { audience });
   }
 }
